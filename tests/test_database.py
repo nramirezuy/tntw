28ad9759d0d536e2a05554f2d3a9aec50dcc2c76
Nicolás Ramírez <nramirez.uy@gmail.com>
2024-11-07 20:10:49 -0300

    test: Verify the database session is healthy

--- tests/test_database.py
+++ tests/test_database.py
@@ -12,3 +12,14 @@ def test_get_session_is_async(minimal_environment):
 @pytest.mark.needs_elasticsearch
 def test_get_session_is_singleton(minimal_environment):
     assert get_session() is get_session()
+
+
+@pytest.mark.needs_elasticsearch
+@pytest.mark.asyncio
+async def test_session_health(minimal_environment):
+    """Verify the session actually works and the cluster is healthy"""
+    client = get_session()
+    response = await client.options(request_timeout=1).cluster.health(
+        wait_for_status="green"
+    )
+    assert response.get("status") == "green"
