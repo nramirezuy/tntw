0440a71f9ad65f253c7c485d9518bddde5aca200
Nicolás Ramírez <nramirez.uy@gmail.com>
2024-11-09 14:28:37 -0300

    style: Add MonkeyPatch annotation

--- tests/unittest/api/v1/conftest.py
+++ tests/unittest/api/v1/conftest.py
@@ -1,6 +1,7 @@
 import pytest
+from pytest import MonkeyPatch
 
 
 @pytest.fixture
-def minimal_environment(monkeypatch) -> None:
+def minimal_environment(monkeypatch: MonkeyPatch) -> None:
     monkeypatch.setenv("TNTW_DATABASE_URL", "http://localhost:9200")
